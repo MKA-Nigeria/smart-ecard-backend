f6da29df0e574d5a201eb73f32518d6eb72c5ef1
YusuffAhmad <yusuffahmad2005@gmail.com>
2024-05-11 18:21:26 +0100

    implement lookups

---
+++ src/Core/Domain/Entities/Lookup.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Common.Contracts;
+
+namespace Domain.Entities;
+public class Lookup : AuditableEntity, IAggregateRoot
+{
+    public string Type { get; set; } = default!;
+    public string Name { get; set; } = default!;
+    public string Value { get; set; } = default!;
+    public bool IsDefault { get; set; }
+
+    public Lookup(string type, string name, string value)
+    {
+        Type = type;
+        Name = name;
+        Value = value;
+        IsDefault = false;
+    }
+
+    public Lookup Update(string? value)
+    {
+        if (value is not null && Value?.Equals(value) is not true) Value = value;
+        return this;
+    }
+
+    public void ToggleDefaultStatus(bool isDefault)
+    {
+        // prevent error while changing to same status
+        if (IsDefault == isDefault) return;
+
+        IsDefault = isDefault;
+    }
+}
---
+++ src/Host/Controllers/Lookups/LookupsController.cs
@@ -0,0 +1,53 @@
+﻿using Application.Common.Models;
+using Application.Lookups.Commands;
+using Application.Lookups.Dtos;
+using Application.Lookups.Queries;
+using Application.Lookups.Validators;
+using Infrastructure.Auth.Permissions;
+using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
+using Shared.Authorization;
+
+namespace Host.Controllers.Lookups;
+public class LookupsController : VersionNeutralApiController
+{
+    [HttpPost]
+    //[MustHavePermission(AppAction.Create, Resource.Lookups)]
+    [OpenApiOperation("Create a new new lookup with unique Id", "")]
+    public Task<Guid> CreateAsync(CreateLookupCommand request)
+    {
+        return Mediator.Send(request);
+    }
+
+    [HttpGet]
+    //[MustHavePermission(AppAction.View, Resource.Lookups)]
+    [OpenApiOperation("Get all application lookups", "")]
+    public Task<PaginationResponse<LookupDto>> GetAppConfigurations()
+    {
+        return Mediator.Send(new GetLookupsQuery());
+    }
+
+    [HttpPut("update")]
+    //[MustHavePermission(AppAction.Update, Resource.Lookups)]
+    [OpenApiOperation("Update the the reqest specified lookup value", "")]
+    public Task<DefaultIdType> UpdateAppConfigurations(UpdateLookupCommand request)
+    {
+        return Mediator.Send(request);
+    }
+
+    [HttpPut("update")]
+    // [MustHavePermission(AppAction.Update, Resource.Lookups)]
+    [OpenApiOperation("Toggle lookup default status", "")]
+    public Task<DefaultIdType> UpdateAppConfigurations(ToggleDefaultStatusCommand request)
+    {
+        return Mediator.Send(request);
+    }
+
+    [HttpGet("{type}")]
+    //[MustHavePermission(AppAction.Search, Resource.Lookups)]
+    [OpenApiOperation("Get application Configurations settings by key", "")]
+    public Task<List<LookupDto>> GetByKey(string type)
+    {
+        return Mediator.Send(new GetLookupsByTypeQuery(type));
+    }
+}

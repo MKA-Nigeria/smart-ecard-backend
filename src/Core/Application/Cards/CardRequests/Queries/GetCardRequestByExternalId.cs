f1ab0aa91b27bd592b48c059406d5ca66dbc12dd
ADEYEMI NURUDEEN OLATUNBOSUN <adeyemisworld@gmail.com>
2024-10-17 18:07:04 +0100

    photo view

--- src/Core/Application/Cards/CardRequests/Queries/GetCardRequestByExternalId.cs
+++ src/Core/Application/Cards/CardRequests/Queries/GetCardRequestByExternalId.cs
@@ -1,5 +1,8 @@
 ﻿using Application.Cards.CardRequests.Queries.Dto;
+using Application.Cards.Cards.Dto;
 using Application.Common.Dtos;
+using Application.Common.FileStorage;
+using Domain.Cards;
 using Domain.Enums;
 using Mapster;
 
@@ -8,11 +11,14 @@ public class GetCardRequestByExternalId : IRequest<BaseResponse<CardRequestDto>>
 {
     public string EntityId { get; set; } = default!;
 }
-public class GetCardRequestByExternalIdHandler(IRepository<CardRequest> repository) : IRequestHandler<GetCardRequestByExternalId, BaseResponse<CardRequestDto>>
+public class GetCardRequestByExternalIdHandler(IRepository<CardRequest> repository, IFileStorageService fileStorageService) : IRequestHandler<GetCardRequestByExternalId, BaseResponse<CardRequestDto>>
 {
     public async Task<BaseResponse<CardRequestDto>> Handle(GetCardRequestByExternalId request, CancellationToken cancellationToken)
     {
         var cardRequest = await repository.GetByExpressionAsync(x => x.ExternalId == request.EntityId && x.Status == CardRequestStatus.Pending, cancellationToken);
+
+        string imageData = await fileStorageService.GetImageDataAsync(cardRequest.CardData.PhotoUrl);
+
         if (cardRequest is null)
         {
             return new BaseResponse<CardRequestDto>
@@ -32,6 +38,7 @@ public class GetCardRequestByExternalIdHandler(IRepository<CardRequest> reposito
         };
         cardRequestDto.MemberData.CustomData = cardRequest.CustomData.ToDictionary();
         cardRequestDto.MemberData.EntityId = cardRequest.ExternalId;
+        cardRequestDto.MemberData.PhotoUrl = imageData ?? null;
         return new BaseResponse<CardRequestDto>
         {
             Data = cardRequestDto,
